27459925fb19614c8449f6b914e2ae446cf7f60d
Aresdgi <115067719+Aresdgi@users.noreply.github.com>
2025-03-06 11:34:25 +0100

    añadido crear tareas y listar tareas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { TaskCreator } from "./components/TaskCreator";
 
 function App() {
+  const [tasksItems, setTasksItems] = useState([
+    { name: "mi primera tarea", done: false },
+    { name: "mi segunda tarea", done: false },
+    { name: "mi tercera tarea", done: false },
+  ]); //crea un estado llamado tasksItems y una funcion para actualizarlo llamada setTasksItems
+
+  function createNewTask(taskName) {
+    if (!tasksItems.find((task) => task.name === taskName)) {
+      setTasksItems([...tasksItems, { name: taskName, done: false }]);
+    }else{
+      alert("Task already exists");
+    }
+
+    //console.log(taskName);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TaskCreator createNewTask={createNewTask} />
+
+      <table>
+        <thead>
+          <tr>
+            <th>Task</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tasksItems.map((task) => (
+            <tr key={task.name}>
+              <td>{task.name}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
---
+++ src/components/TaskCreator.js
@@ -0,0 +1,25 @@
+import { useState } from "react"; //importa useState de react
+
+export const TaskCreator = ({ createNewTask }) => {
+  //crea un componente llamado TaskCreator que recibe las props
+  const [newTaskName, setNewTaskName] = useState(""); //crea un estado llamado newTaskName y una funcion para actualizarlo llamada setNewTaskName
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    createNewTask(newTaskName); //ejecuta la funcion createNewTask que viene en las props de TaskCreator
+    localStorage.setItem("task", newTaskName); //guarda el valor de newTaskName en el local storage
+    setNewTaskName(""); //resetea el valor de newTaskName
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Enter a new task"
+        value={newTaskName} //el valor del input es igual al estado newTaskName
+        onChange={(e) => setNewTaskName(e.target.value)} // esta funcion se ejecuta cada vez que el usuario escribe en el input y actualiza el estado de newTaskName
+      />
+
+      <button>Save Task</button>
+    </form>
+  );
+};
